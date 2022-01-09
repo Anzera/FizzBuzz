ebbdb85d57b1fece29c6c1f509dd4d6601a6bafe
Anna Frączek <aniabaj@gmail.com>
2022-01-09 22:12:18 +0100

    remove  'else' statment

--- ZadanieDomowe4/FizzBuzz.cs
+++ ZadanieDomowe4/FizzBuzz.cs
@@ -29,12 +29,12 @@ namespace ZadanieDomowe4
         {
             if (number % 3 == 0 && number % 5 == 0)
                 return "FizzBuzz";
-            else if (number % 3 == 0)
+            if (number % 3 == 0)
                 return "Fizz";
-            else if (number % 5 == 0)
+            if (number % 5 == 0)
                 return "Buzz";
-            else
-                return number.ToString();
+           
+            return number.ToString();
         }
     }
 }
