0bac2c2943fab8f73b121b2c5c7219de36a27e00
Anna Frączek <aniabaj@gmail.com>
2022-01-09 22:04:07 +0100

    add class FizzBuzz, methods GetNumber and IsFizzBuzz

--- ZadanieDomowe4/FizzBuzz.cs
+++ ZadanieDomowe4/FizzBuzz.cs
@@ -8,5 +8,33 @@ namespace ZadanieDomowe4
 {
     class FizzBuzz
     {
+        public int GetNumber()
+        {    
+            while (true)
+            {
+                var val = Console.ReadLine();
+                if (val.ToUpper() == "T")
+                    Environment.Exit(0); 
+                
+                if (!int.TryParse(val, out int number))
+                {
+                    Console.WriteLine("Wprowadziłeś złe dane. Wprowadź liczbę:");
+                    continue;
+                }
+                return number;
+            }
+        }
+
+        public string IsFizzBuzz(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+                return "FizzBuzz";
+            else if (number % 3 == 0)
+                return "Fizz";
+            else if (number % 5 == 0)
+                return "Buzz";
+            else
+                return $"{number}";
+        }
     }
 }
--- ZadanieDomowe4/Program.cs
+++ ZadanieDomowe4/Program.cs
@@ -10,6 +10,15 @@ namespace ZadanieDomowe4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Witaj w grze FizzBuzz! Zasady są proste:\nJeżeli wpiszesz liczbę podzielną przez 3 na ekarnie wyświetli się słowo 'Fizz'\nJeżeli wprowadzona liczba będzie podzielna przez 5 to na ekranie pojawi się słowo 'Buzz'\nJeżeli liczba będzie podzielna zarówno przez 3 i 5 na ekranie wyświetli się 'FizzBuzz'\nW przypadku gdy liczba nie będzie podzielna ani przez 3, ani przez 5 to wyświetli się wpisana przez Ciebie liczba.\n\nZaczynajmy! Wpisz liczbę:");
+            FizzBuzz fizzBuzz = new FizzBuzz();
+
+            while (true)
+            {
+                var number = fizzBuzz.GetNumber();
+                Console.WriteLine(fizzBuzz.IsFizzBuzz(number));
+                Console.WriteLine("Jeśli chcesz zakończyć wciśnij 't', w przeciwnym wypadku podaj kolejną liczbę:");
+            }
         }
     }
 }
